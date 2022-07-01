7fb17ce01ad3b70cfad97e547354806632c68e74
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-07-01 10:05:34 -0700

    applied create appt with the new appt and saving it as json

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,6 +1,8 @@
 =begin
 return all appointments in the appt contraoller
 need to create new appt where there is a patient, dr, starttime, dur
+find patient/dr where their name meets parameters of patient
+
 save appt through render json if error status 401
 =end
 class Api::AppointmentsController < ApplicationController
@@ -11,12 +13,16 @@ class Api::AppointmentsController < ApplicationController
   end
 
   def create
-  #   appointment = Appointment.new(
-  #     patient_id: { name: <string> },
-  #     doctor_id : { name: <string>, id: <int> },
-  #     created_at: <iso8601>,
-  #     start_time: <iso8601>,
-  #     duration_in_minutes: <int>
-  #   )
-  # end
+    @appointment = Appointment.new(
+      patient_id: Patient.where(name: params[:patient][:name][:id]),
+      doctor_id: params[:doctor][:id],
+      start_time: params[:start_time],
+      duration_in_minutes: params[:duration_in_minutes]
+    )
+    if @appointment.save
+      render json: @appointment
+    else
+      render json: {status: 401}
+    end
+  end
 end
