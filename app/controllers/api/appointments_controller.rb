e0d39825f4d374f01d85de8d4c4034455b381f5e
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-07-01 09:52:11 -0700

    addition of pseudocode and endpoint structure

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,3 +1,8 @@
+=begin
+return all appointments in the appt contraoller
+need to create new appt where there is a patient, dr, starttime, dur
+save appt through render json if error status 401
+=end
 class Api::AppointmentsController < ApplicationController
   def index
     # TODO: return all values
@@ -6,6 +11,12 @@ class Api::AppointmentsController < ApplicationController
   end
 
   def create
-    # TODO:
-  end
+  #   appointment = Appointment.new(
+  #     patient_id: { name: <string> },
+  #     doctor_id : { name: <string>, id: <int> },
+  #     created_at: <iso8601>,
+  #     start_time: <iso8601>,
+  #     duration_in_minutes: <int>
+  #   )
+  # end
 end
