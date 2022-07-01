6a5c3e653d711372f0f4808212656798e546a6da
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-06-30 18:36:28 -0700

    removed pseudocode and doctor email as instructions did not state it was needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,6 @@
 # -3. Each patient should have 10 appointments (5 in the past, 5 in the future)
 #   -3.5 Each appointment should be 50 minutes in duration
 
-# pseudocode - for every doctor/patient create a unique name using faker
-# to generate unique fake data (loop through)
-# utilize rand() to create random dates in time past / future, 365 days over 2 years
-# within code block of doctor add patient - within code block of patient add
-# separate appointment blocks
-
 Doctor.destroy_all
 Patient.destroy_all
 Appointment.destroy_all
@@ -17,8 +11,6 @@ Appointment.destroy_all
 10.times do
   doctor = Doctor.create(
     name: Faker::Name.unique.name
-    email: Faker::Email.unique.email
-
   )
 
   10.times do
