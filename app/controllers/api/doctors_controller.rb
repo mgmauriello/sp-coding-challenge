1436119d1427a9525acd2177cbc86167630b19ad
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-07-01 23:14:32 -0700

    removed pseudocode

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -1,10 +1,3 @@
-=begin
-Create a new endpoint that returns all doctors that
-do not have an appointment.
-need to create class dedicated for the doctors controller in the appcont
-define doctors where doctors do not match an appointment at the dr id
-  return doctors
-=end
 class Api::DoctorsController < ApplicationController
   def index
     # returns every doctor without an appointment scheduled
