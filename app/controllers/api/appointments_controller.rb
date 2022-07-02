e097ef06e482895188015ae8644feed0282d216b
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-07-02 09:50:20 -0700

    readded spec controller api - need to code through the make models run, fixed post request based on new pagination tutorial, addition of env and rspec based on errors found in testing

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -2,29 +2,30 @@ class Api::AppointmentsController < ApplicationController
   def index
     # filter for past, future, and all appointments
     if params[:past] == "1"
-      @appointments = Appointment.where("start_time < ?", Time.zone.now)
-    elsif params [:past] == "0"
-      @appointments = Appointment.where("start_time > ?", Time.zone.now)
+      appointments = Appointment.where("start_time < ?", Time.zone.now)
+    elsif params[:past] == "0"
+      appointments = Appointment.where("start_time > ?", Time.zone.now)
+    #  paginated appointments
+    elsif params[:page] && params[:length]
+      limit = params[:length]
+      offset = params[:page] - 1 * params[:length]
+      appointments = Appointment.limit(limit).offset(offset)
     else
-      @appointment = Appointment.all
-      render json: @appointment
+      appointments = Appointment.all
     end
-    @appointments = appointments.order(start_time: :asc).limit(params[:length]).offset(params[:page])
-    render json: @appointments
-  end
 
   def create
-    @appointment = Appointment.new(
+    appointment = Appointment.new(
       patient_id: Patient.where(name: params[:patient][:name]),
       doctor_id: params[:doctor][:id],
       start_time: params[:start_time],
       duration_in_minutes: params[:duration_in_minutes]
     )
     # save new appointment
-    if @appointment.save
-      render json: @appointment, status: :created, location: @appointment
+    if appointment.save
+      render json: appointment, status: :created, location: appointment
     else
-      render json: @appointment.errors, status: :unprocessable_entity
+      render json: appointment.errors, status: :unprocessable_entity
     end
   end
 end
