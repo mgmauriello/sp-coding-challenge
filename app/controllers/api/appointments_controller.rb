8191f4fc3a5a65718afa33c13b7a7b02f31fc110
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-07-01 22:53:33 -0700

    pagintation of page and length while ordering via start time

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -7,9 +7,15 @@ for filter: if past == 1, return the key [:past] appts where the
   appt start time less than the point of origin at the current time
   if past == 0, return future appts where the appt start time
     greater than the point of origin at the current time
+  return paginated appts, using limit method to limit
+    to tell the number of records to be fetched, and use offset
+      to tell the number of records to skip before starting to
+      return the records.
+  limit per page, offset page
 =end
 class Api::AppointmentsController < ApplicationController
   def index
+    # filter for past, future, and all appointments
     if params[:past] === 1
       @appointments = Appointment.where("start_time < ?", Time.zone.now)
     elsif params [:past] === 0
@@ -18,11 +24,7 @@ class Api::AppointmentsController < ApplicationController
       @appointment = Appointment.all
       render json: @appointment
     end
-
-  end
-
-  def show
-    render json: @appointment
+    @appointments = appointments.order(start_time: :asc).limit(params[:length]).offset(params[:page])
   end
 
   def create
@@ -32,6 +34,7 @@ class Api::AppointmentsController < ApplicationController
       start_time: params[:start_time],
       duration_in_minutes: params[:duration_in_minutes]
     )
+    # save new appointment as json or return error
     if @appointment.save
       render json: @appointment, status: :created, location: @appointment
     else
